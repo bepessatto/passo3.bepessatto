71c801bd409e5dfdaf2a3d934a918764b6d73100
bernardo <bernardo.malaggi@gmail.com>
2023-10-09 08:56:19 -0300

    passo 3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const app = express();
+app.use('/api', router);
 
+const router = require('./routes/rotas');
 app.set('port', process.env.PORT || 3333);
 
 module.exports = app;
